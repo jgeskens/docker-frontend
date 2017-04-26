a3067ca02f5eab93fb4f4027958abb1b22cff28f
Dirk Moors <dirkmoors@gmail.com>
2017-04-26 11:19:23 +0200

    Updated chown-logic

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,6 +6,8 @@
 
 USER_ID=${LOCAL_USER_ID:-0}
 USERNAME=${LOCAL_USERNAME:-user}
+CHOWNDIRS=${CHOWNDIRS}
+VERBOSE=${VERBOSE}
 
 case ${USER_ID} in
    "0")
@@ -15,7 +17,18 @@ case ${USER_ID} in
    *)
         # Run as non-root
         adduser -s /bin/bash -u ${USER_ID} -D ${USERNAME}
-        chown -R ${USER_ID}:${USER_ID} ${SRC_DIR}
+
+        # chown dirs
+        if [ "${CHOWNDIRS}" ];
+        then
+            IFS=',' read -r -a array <<< "${CHOWNDIRS}"
+            for element in "${array[@]}"
+            do
+                if [ "${VERBOSE}" ]; then printf "transfering directory ownership for ${element} to ${USER_ID}:${USER_ID} ...\n"; fi
+                chown -R ${USER_ID}:${USER_ID} ${element};
+            done
+        fi
+
         exec su-exec ${USERNAME} "$@"
         ;;
 esac
